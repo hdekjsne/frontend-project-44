933c277069cf9034543a1367d31173139a4ac441
hdekjsne <hdekjsne@gmail.com>
2022-11-29 14:20:34 +0300

    add progression-game

--- README.md
+++ README.md
@@ -3,8 +3,14 @@
 
 <a href="https://codeclimate.com/github/hdekjsne/frontend-project-44/maintainability"><img src="https://api.codeclimate.com/v1/badges/7bf96b8049787005daa7/maintainability" /></a>
 
+######isEven
 <a href="https://asciinema.org/a/givsH5K9E6NqdstzbjjtDfrfc" target="_blank"><img src="https://asciinema.org/a/givsH5K9E6NqdstzbjjtDfrfc.svg" /></a>
 
+######calculator
 <a href="https://asciinema.org/a/0Nd3JRMhWZBIsqXnLBpEMNNqx" target="_blank"><img src="https://asciinema.org/a/0Nd3JRMhWZBIsqXnLBpEMNNqx.svg" /></a>
 
+######greatest common dividor
 <a href="https://asciinema.org/a/y1DV9DL6QJwv2VKiR3WXLZ5Wu" target="_blank"><img src="https://asciinema.org/a/y1DV9DL6QJwv2VKiR3WXLZ5Wu.svg" /></a>
+
+######progression
+<a href="https://asciinema.org/a/v4qMi9Nv6mHlEH19yJ2dyFLip" target="_blank"><img src="https://asciinema.org/a/v4qMi9Nv6mHlEH19yJ2dyFLip.svg" /></a>
---
+++ bin/brain-progression.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+let name = readlineSync.question('May I have your name? ');
+console.log('Hello, ' + name + '!');
+
+function progression () {
+    let correctCount = 0;
+    while (correctCount < 3) {
+        const firstNum = Math.floor(Math.random() * 20);
+        const diff = Math.floor(Math.random() * 30);
+
+        let numbers = [];
+        numbers.push(firstNum);
+        for (let i = 1; i < 4; i++) {
+            numbers.push(numbers[i - 1] + diff);
+        }
+        
+        const gapIndex = Math.floor(Math.random() * 4);
+        let missing = numbers[gapIndex];
+        numbers[gapIndex] = '...'
+
+        console.log('Question: ' + numbers.join(', '));
+
+        const answer = readlineSync.question('Answer: ');
+
+        if (Number(answer) === missing) {
+            correctCount++;
+            console.log('Correct!');
+        } else {
+            console.log('Answer was ' + missing + '. Try again, ' + name + '. You can do better!');
+        }
+    }
+    console.log('Congratulation, ' + name + '!');
+}
+
+progression();
