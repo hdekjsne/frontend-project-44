74e1830902528df72d74c9a2ea5d7d5dcc22ec23
hdekjsne <hdekjsne@gmail.com>
2022-11-29 10:33:46 +0300

    add new game

---
+++ bin/brain-calc.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+
+let name = readlineSync.question('May I have your name? ');
+console.log('Hello, ' + name + '!');
+
+console.log('What is the result of the expression?');
+
+function calc () {
+    const operators = ['+', '-', '*'];
+    let correctCount = 0;
+    while (correctCount < 3) {
+        let randomOper = operators[Math.floor(Math.random() * 3)];
+        let first = Math.round(Math.random() * 100);
+        let second = Math.round(Math.random() * 100);  
+        let expression = first + ' ' + randomOper + ' ' + second; 
+        console.log('Question: ' + expression);
+        let answer = readlineSync.question('Your answer: ');
+        switch (randomOper) {
+            case '+':
+                if (Number(answer) === Number(first) + Number(second)) {
+                    correctCount++;
+                    console.log('Correct!');
+                } else {
+                    console.log(answer + ' is wrong answer ;(. Correct answer was ' + (Number(first) + Number(second)));
+                }        
+                break;
+
+            case '-':
+                if (Number(answer) === Number(first) - Number(second)) {
+                    correctCount++;
+                    console.log('Correct!');
+                } else {
+                    console.log(answer + ' is wrong answer ;(. Correct answer was ' + (Number(first) - Number(second)));
+                }
+                break;
+
+            case '*':
+                if (Number(answer) === Number(first) * Number(second)) {
+                    correctCount++;
+                    console.log('Correct!');
+                } else {
+                    console.log(answer + ' is wrong answer ;(. Correct answer was ' + (Number(first) * Number(second)))
+                }
+                break;
+        }
+    }
+    console.log('Congratulations, ' + name + '!')
+}
+
+calc();
